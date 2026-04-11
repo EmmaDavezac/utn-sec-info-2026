2b80148d81832c8e95a3567fce4eb62149014f49
Emmanuel <lucianodavezac@gmail.com>
2026-04-10 21:09:53 -0300

    correccion en auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth";
-import { authOptions } from "@/app/lib/auth"; // Importas la configuración desde tu librería
+import { authOptions } from "@/app/lib/auth";
+import { initDb } from "@/app/lib/db";
 
-const handler = NextAuth(authOptions);
+const initPromise = initDb();
+
+async function handler(req: Request, ctx: unknown) {
+  await initPromise;
+  const nextAuthHandler = NextAuth(authOptions);
+  return nextAuthHandler(req, ctx);
+}
 
 export { handler as GET, handler as POST };
