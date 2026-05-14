ae84d583ada6a8fb0b8534acecddd23eb2c0f23c
Emmanuel <lucianodavezac@gmail.com>
2026-05-14 19:23:18 -0300

    correcciones de cambios en la demostracion en clase

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -8,7 +8,6 @@ export default async function AdminPage() {
 
   if (!session) {
     redirect("/auth")
-    console.log("test")
   }
 
   const role = ((session.user as any)?.role as string | undefined)?.trim().toLowerCase() ?? ""
